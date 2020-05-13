4712682f55610c4ea5a35d522b0b2e1947cbc6dc
Pande Putu Widya Oktapratama <widya.oktapratama@gmail.com>
2020-05-13 13:36:48 +0800

    Add facade to register

--- src/SignerServiceProvider.php
+++ src/SignerServiceProvider.php
@@ -3,11 +3,16 @@
 namespace Izidev\Signer;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Iziedev\Signer\Signer;
 
 class SignerServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        App::bind('iziedev.signer', function () {
+            return new Signer;
+        });
     }
 
     public function boot()
